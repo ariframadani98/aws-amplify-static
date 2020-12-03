b275c0cbf9612a6c22229740a612d11915b00ea3
ariframadani98 <75296864+ariframadani98@users.noreply.github.com>
2020-12-03 15:38:57 +0700

    smkku

--- js/custom.js
+++ js/custom.js
@@ -1,204 +1,107 @@
-/******************************************
-    File Name: custom.js
-*******************************************/
-
-"use strict";
-
-/**== wow animation ==**/
-
-new WOW().init();
-
-/**== loader js ==*/
-
-$(window).load(function() {
-    $(".bg_load").fadeOut("slow");
-})
-
-/**== Menu js ==**/
-
-$("#navbar_menu").menumaker({
-	title: "Menu",
-	format: "multitoggle"
-});
-
-/** counter js **/
-
-    $('.counter-count').each(function () {
-        $(this).prop('Counter',0).animate({
-            Counter: $(this).text()
-        }, {
-            duration: 5000,
-            easing: 'swing',
-            step: function (now) {
-                $(this).text(Math.ceil(now));
-            }
-        });
-    });
-
-/** progress_bar js **/
+(function($) {
+    "use strict";
 	
-	 $(document).ready(function() {
-      $('.progress .progress-bar').css("width",
-                function() {
-                    return $(this).attr("aria-valuenow") + "%";
-                }
-        )
-    });
+	/* ..............................................
+	Loader 
+    ................................................. */
 	
-/** Casestudies Tab_bar js **/	
+	$(window).on('load', function() { 
+		$('.preloader').fadeOut(); 
+		$('#preloader').delay(550).fadeOut('slow'); 
+		$('body').delay(450).css({'overflow':'visible'});
+	});
+    	
+	/* ..............................................
+    Navbar Bar
+    ................................................. */
 	
-	$(document).ready(function(){
-
-    $(".filter-button").click(function(){
-        var value = $(this).attr('data-filter');
-        
-        if(value == "all")
-        {
-            //$('.filter').removeClass('hidden');
-            $('.filter').show('1000');
-        }
-        else
-        {
-//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
-//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
-            $(".filter").not('.'+value).hide('3000');
-            $('.filter').filter('.'+value).show('3000');
-            
-        }
-    });
+	$('.navbar-nav .nav-link').on('click', function() {
+		var toggle = $('.navbar-toggler').is(':visible');
+		if (toggle) {
+			$('.navbar-collapse').collapse('hide');
+		}
+	});
+	
+	/* ..............................................
+    Fixed Menu
+    ................................................. */
     
-    if ($(".filter-button").removeClass("active")) {
-$(this).removeClass("active");
-}
-$(this).addClass("active");
+	$(window).on('scroll', function () {
+		if ($(window).scrollTop() > 50) {
+			$('.top-header').addClass('fixed-menu');
+		} else {
+			$('.top-header').removeClass('fixed-menu');
+		}
+	});
 
-});
+	/* ..............................................
+    Properties Filter
+    ................................................. */
+	var Container = $('.container');
+	Container.imagesLoaded(function () {
+		var portfolio = $('.properties-menu');
+		portfolio.on('click', 'button', function () {
+			$(this).addClass('active').siblings().removeClass('active');
+			var filterValue = $(this).attr('data-filter');
+			$grid.isotope({
+				filter: filterValue
+			});
+		});
+		var $grid = $('.properties-list').isotope({
+			itemSelector: '.properties-grid'
+		});
 
-/** google_map js **/
+	});
 
-function myMap() {
-var mapProp= {
-    center:new google.maps.LatLng(40.712775,-74.005973),
-    zoom:18,
-};
-var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-}
+	/* ..............................................
+    Gallery
+    ................................................. */
+	
+	$(document).ready(function() {
+		$('.popup-gallery').magnificPopup({
+			delegate: 'a',
+			type: 'image',
+			tLoading: 'Loading image #%curr%...',
+			mainClass: 'mfp-img-mobile',
+			gallery: {
+				enabled: true,
+				navigateByImgClick: true,
+				preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+			},
+			image: {
+				tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+				titleSrc: function(item) {
+					return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+				}
+			}
+		});
+	});
+	
+	/* ..............................................
+    Scroll To Top
+    ................................................. */
+	
+	$(document).ready(function () {
 
-/**===== Slider =====**/
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 100) {
+				$('#scroll-to-top').fadeIn();
+			} else {
+				$('#scroll-to-top').fadeOut();
+			}
+		});
 
-var tpj = jQuery;
-var revapi4;
-tpj(document).ready(function() {
-    if (tpj("#rev_slider_4_1").revolution == undefined) {
-        revslider_showDoubleJqueryError("#rev_slider_4_1");
-    } else {
-        revapi4 = tpj("#rev_slider_4_1").show().revolution({
-            sliderType: "standard",
-            jsFileLocation: "revolution/js/",
-            sliderLayout: "fullwidth",
-            dottedOverlay: "none",
-            delay: 7000,
-            navigation: {
-                keyboardNavigation: "off",
-                keyboard_direction: "horizontal",
-                mouseScrollNavigation: "off",
-                onHoverStop: "off",
-                touch: {
-                    touchenabled: "on",
-                    swipe_threshold: 75,
-                    swipe_min_touches: 1,
-                    swipe_direction: "horizontal",
-                    drag_block_vertical: false
-                },
-                arrows: {
-                    style: "zeus",
-                    enable: true,
-                    hide_onmobile: true,
-                    hide_under: 600,
-                    hide_onleave: true,
-                    hide_delay: 200,
-                    hide_delay_mobile: 1200,
-                    tmp: '<div class="tp-title-wrap"><div class="tp-arr-imgholder"></div></div>',
-                    left: {
-                        h_align: "left",
-                        v_align: "center",
-                        h_offset: 30,
-                        v_offset: 0
-                    },
-                    right: {
-                        h_align: "right",
-                        v_align: "center",
-                        h_offset: 30,
-                        v_offset: 0
-                    }
-                },
-                bullets: {
-                    enable: true,
-                    hide_onmobile: true,
-                    hide_under: 600,
-                    style: "metis",
-                    hide_onleave: true,
-                    hide_delay: 200,
-                    hide_delay_mobile: 1200,
-                    direction: "horizontal",
-                    h_align: "center",
-                    v_align: "bottom",
-                    h_offset: 0,
-                    v_offset: 30,
-                    space: 5,
-                    tmp: '<span class="tp-bullet-img-wrap">  <span class="tp-bullet-image"></span></span><span class="tp-bullet-title">{{title}}</span>'
-                }
-            },
-            viewPort: {
-                enable: true,
-                outof: "pause",
-                visible_area: "80%"
-            },
-            responsiveLevels: [1240, 1024, 778, 480],
-            gridwidth: [1240, 1024, 778, 480],
-            gridheight: [700, 700, 500, 400],
-            lazyType: "none",
-            parallax: {
-                type: "mouse",
-                origo: "slidercenter",
-                speed: 2000,
-                levels: [2, 3, 4, 5, 6, 7, 12, 16, 10, 50],
-            },
-            shadow: 0,
-            spinner: "off",
-            stopLoop: "off",
-            stopAfterLoops: -1,
-            stopAtSlide: -1,
-            shuffle: "off",
-            autoHeight: "off",
-            hideThumbsOnMobile: "off",
-            hideSliderAtLimit: 0,
-            hideCaptionAtLimit: 0,
-            hideAllCaptionAtLilmit: 0,
-            debugMode: false,
-            fallbacks: {
-                simplifyAll: "off",
-                nextSlideOnWindowFocus: "off",
-                disableFocusListener: false,
-            }
-        });
-    }
-});
+		$('#scroll-to-top').click(function () {
+			$("html, body").animate({
+				scrollTop: 0
+			}, 600);
+			return false;
+		});
 
-/**===== End slider =====**/
+	});
 	
 	
-/** header fixed js **/
-
-$(window).scroll(function(){
-    if ($(window).scrollTop() >= 300) {
-       $('.header_fixed_on_scroll').addClass('fixed-header');
-    }
-    else {
-       $('.header_fixed_on_scroll').removeClass('fixed-header');
-    }
-});
 
 	
-	
-	
+}(jQuery));
+
